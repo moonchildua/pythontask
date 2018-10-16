96d1ab5d3601d4fac48dea9b6bcf07cf48f1b8ba
Ignite Admin <github@igniteoutsourcing.com>
2018-10-16 14:18:58 +0300

    first commit

---
+++ 2.2.py
@@ -0,0 +1,83 @@
+# #n += 1
+# n = 1
+# while n < 5:
+#     n += 1
+#     print(n**2)
+#
+#
+# for n in [1,2,3,4,5]:
+#     print(n, "**2", n**2)
+
+# for animals in ["cat", "dog", "elephant"]:
+#     print(animals)
+
+# for animal in ["cat", "dog", "elephant"]:
+#     if animal == "cat":
+#         print("Hello", animal)
+#     else:
+#         print("go away!", animal)
+
+
+# rng = range(6)
+# print(list(rng))
+
+# rng = range(3, 6, 2)
+# print(list(rng))
+
+# for num in range(1, 6):
+#     print(num**2)
+
+#Home Work 1
+# n = int (input("Please write number, "))
+# if n%3 == 0 and n%5 == 0:
+#     print("Fizz Buzz")
+# if n%3 == 0:
+#     print("Fizz")
+# elif n%5 == 0:
+#     print("Buzz")
+# else:
+#     print("n= ", n)
+
+#Home Work 1
+# n = int (input("please enter number "))
+# def square_event ():
+#     lst = []
+#     for num in range (1, n+1):
+#         if num % 2 == 0:
+#             lst.append(num**2)
+# #  collumn          num=num**2
+# #  collumn          print(num)
+#     print(lst)
+# square_event()
+
+# Simple function without condition
+# def index (m, n):
+#         num_squar = m[n] ** n
+#         print(num_squar)
+#
+#
+# n = int (input("Please write number, "))
+# m = [1, 3, 10, 100]
+# index ( m, n)
+
+
+# def index (m, n):
+#     if m[n] is False:
+#         n= -1
+#         return n
+#     else:
+#         num_squar = m[n] ** n
+#         print(num_squar)
+#
+#
+# n = int (input("Please write number, "))
+# m = [0, 1]
+# index ( m, n)
+
+#
+# index_power = ([1, 2, 3, 4], n)
+#index_power = ([1, 3, 10, 100], n )
+# index_power = ([0, 1], n)
+# index_power = ([1, 2], n)
+
+
