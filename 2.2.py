415e0d99641e3124cbea6fe6a6732dfaf4dd60fb
Ignite Admin <github@igniteoutsourcing.com>
2018-10-17 12:40:14 +0300

    home work - done

--- 2.2.py
+++ 2.2.py
@@ -27,7 +27,7 @@
 # for num in range(1, 6):
 #     print(num**2)
 
-#Home Work 1
+# Home Work 1
 # n = int (input("Please write number, "))
 # if n%3 == 0 and n%5 == 0:
 #     print("Fizz Buzz")
@@ -38,7 +38,7 @@
 # else:
 #     print("n= ", n)
 
-#Home Work 1
+# Home Work 1
 # n = int (input("please enter number "))
 # def square_event ():
 #     lst = []
@@ -60,24 +60,27 @@
 # m = [1, 3, 10, 100]
 # index ( m, n)
 
+# thing_index = thing_list.index(elem) if elem in thing_list else -1
 
-# def index (m, n):
-#     if m[n] is False:
-#         n= -1
-#         return n
-#     else:
-#         num_squar = m[n] ** n
-#         print(num_squar)
-#
-#
-# n = int (input("Please write number, "))
-# m = [0, 1]
-# index ( m, n)
+def index (m,n):
+    # if m[n] is True:
+    #     num_squar = m[n] ** n
+    #     print (num_squar)
+    if len(m) <= n :
+        num_squar = -1
+        print(num_squar)
+    else:
+        num_squar = m[n] ** n
+        print(num_squar)
 
-#
+
+
+n = int(input("Please write number, "))
+m = [1, 2]
+index(m,n)
+
+# except IndexError:
 # index_power = ([1, 2, 3, 4], n)
-#index_power = ([1, 3, 10, 100], n )
+# index_power = ([1, 3, 10, 100], n )
 # index_power = ([0, 1], n)
 # index_power = ([1, 2], n)
-
-
