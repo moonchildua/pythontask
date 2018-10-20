602abe24a4974752f468e14794b0505cf11dea83
moonchildua <daswko@gmail.com>
2018-10-20 08:41:47 +0300

    while, for, random

---
+++ 2.2.py
@@ -0,0 +1,21 @@
+# #n += 1
+n = 0
+while n < 5:
+    print(n**2)
+    n += 1
+
+
+# for n in [1,2,3,4,5]:
+#     print(n, "**2", n**2)
+
+# for animals in ["cat", "dog", "elephant"]:
+#     print(animals)
+
+# for animal in ["cat", "dog", "elephant"]:
+#     if animal == "cat":
+#         print("Hello", animal)
+#     else:
+#         print("go away!", animal)
+
+for num in range(1, 500):
+        print (num ** 2)
