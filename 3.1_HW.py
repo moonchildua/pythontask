14ecc4019b981a27ac667b44efd789b8c4c337b3
moonchildua <daswko@gmail.com>
2018-10-25 09:02:47 +0300

    hW 1st commit

---
+++ 3.1_HW.py
@@ -0,0 +1,23 @@
+#
+#
+#
+#
+# (5,3,22)
+# (6,2,11)
+# (4, 4, 7)
+
+
+
+#def kubik ():
+from random import randint
+k = 1
+while True:
+    if sum != 8:
+        n = randint(1, 6)
+        m = randint(1, 6)
+        sum = n + m
+        k += k
+    else:
+       print('sum= ', n + m, n, m, k)
+
+
