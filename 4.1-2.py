778671492d3ef634851fa8f57619d884560156cc
moonchildua <daswko@gmail.com>
2018-11-26 14:46:14 +0200

    list

---
+++ 4.1-2.py
@@ -0,0 +1,41 @@
+# print('"Isn\'t", she said.')
+# print("Hello\nworld")
+# print('c:\some\name')
+# print(r'c:\some\name')
+
+#print('3'+ ' ' + '3')
+
+#word = "Python"
+#assert word[0] == "P"
+#assert word[2:5] == "tho"
+#assert word[-2] == "o"
+#assert word[0:2] == "Py"
+#assert word[4:] == "on"
+#assert word[:2] + word[2:] == "Python"
+#assert word[4:42] == "on"
+#assert word[42:] == "on"
+
+# for letter in "Python":
+#     print(letter)
+
+# for letter in "Python":
+#     print(letter, end="\n")
+
+# suquares = [1, 3, 9, 16, 25]
+
+#assert suquares [0] == 1
+#assert suquares [0] == 2
+
+# assert  suquares [-1] == 25
+#
+# cubes = [1, 8, 27, 65, 125]
+# assert  cubes + [36, 55] == [1, 8, 27, 65, 125, 36, 55]
+#
+# cubes = [1, 8, 27, 65, 125]
+# cubes [3] == 4**3 # замена по елементу индекса
+# assert  cubes == []
+
+
+#print([1+2]+ [3+4])
+
+
