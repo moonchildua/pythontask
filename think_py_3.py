0a456e651862d878732d4a8a46d66d7e4a437b3f
moonchildua <daswko@gmail.com>
2018-11-26 14:46:50 +0200

    task 1 from book

---
+++ think_py_3.py
@@ -0,0 +1,53 @@
+# def print_lyrics():
+#     print("I'm a lumberjack, and I'm okay.")
+#     print("I sleep all night and I work all day.")
+#
+# def repeat_lyrics():
+#     print_lyrics()
+#     print_lyrics()
+#
+# repeat_lyrics()
+# print_lyrics()
+
+
+# def print_twice(bruce):
+#     print(bruce)
+#     print(bruce)
+#
+#
+# print_twice('spam')
+
+
+# def print_twice(bruce):
+#     print(bruce)
+#     print(bruce)
+#
+#
+# print_twice('spam ' *4)
+
+# from math import cos
+# from  math import pi
+#
+# def print_twice(bruce):
+#     print(bruce)
+#     print(bruce)
+#
+# print_twice(cos(pi))
+
+def print_twice(bruce):
+    print(bruce)
+    print(bruce)
+#
+#
+# michael = "hello my ..."
+# print_twice(michael)
+
+
+def cat_twice(part1, part2):
+    cat = part1 + part2
+    print_twice(cat)
+
+
+line1 = 'Bing tiddle '
+line2 = 'tiddle bang.'
+cat_twice(line1, line2)
