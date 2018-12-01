4819fc07686f3a661cc631dbb411361dba6265e7
moonchildua <daswko@gmail.com>
2018-12-01 21:03:12 +0200

    update

--- 4.1-2.py
+++ 4.1-2.py
@@ -1,30 +1,4 @@
-# print('"Isn\'t", she said.')
-# print("Hello\nworld")
-# print('c:\some\name')
-# print(r'c:\some\name')
 
-#print('3'+ ' ' + '3')
-
-#word = "Python"
-#assert word[0] == "P"
-#assert word[2:5] == "tho"
-#assert word[-2] == "o"
-#assert word[0:2] == "Py"
-#assert word[4:] == "on"
-#assert word[:2] + word[2:] == "Python"
-#assert word[4:42] == "on"
-#assert word[42:] == "on"
-
-# for letter in "Python":
-#     print(letter)
-
-# for letter in "Python":
-#     print(letter, end="\n")
-
-# suquares = [1, 3, 9, 16, 25]
-
-#assert suquares [0] == 1
-#assert suquares [0] == 2
 
 # assert  suquares [-1] == 25
 #
@@ -35,7 +9,58 @@
 # cubes [3] == 4**3 # замена по елементу индекса
 # assert  cubes == []
 
+#a = 3
+#print([a])
+#print([1, 2]+ [a])
 
 #print([1+2]+ [3+4])
 
+#написать функцию которая добавляет заданный элемент в конец списка и возварщает этот список
+
+# def adding__element ():
+#     lst = [1, 2, 3]
+#     new_lst = lst + [n]
+#     print(new_lst)
+#     return (new_lst)
+#
+# n = int(input("input n "))
+# adding__element()
+
+
+# def adding__element (lst, element):
+#     return lst + [element]
+#
+# print(adding__element([1, 2, 3],3))
+
+
+
+# def adding__element (input_list, element):  # передаем значение
+#     converted_to_list = [element]           # переменнойconverted_to_list присваиваем  значение element
+#     lst_result = input_list + converted_to_list #  сделали действие над входным списком и елементом
+#     return lst_result  # вернули результат
+#     #return lst + [element] - это кратко без 3 предыдущих строк
+#
+# initial_lst = [1, 2, 3]
+# add_element = 77
+#
+# result_list = adding__element(initial_lst, add_element)
+#
+# #print(result_list)
+# for num in result_list:
+#     print(num)
+
+def count_element (input_list):
+    """
+    [1,2,3] -> 3
+    ['a'] ->1
+    ['a', 'b'] -> 2
+    :param input_list: list
+    :return: int
+    """
+    counter = 0
+    for dummy_i in input_list:
+        counter +=1
+    return counter
+
+print(count_element([1, 2, 3, 7, 8]))
 
